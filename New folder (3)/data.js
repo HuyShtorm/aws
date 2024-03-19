b4e4b571d8b978b9be5572cdd14ed68b4a85f7a4
HuyShtorm <94543868+HuyShtorm@users.noreply.github.com>
2024-03-19 08:32:39 +0700

    Add files via upload

---
+++ New folder (3)/data.js
@@ -0,0 +1,38 @@
+const courses = [{
+        id: 1,
+        name: 'CSDL',
+        courses_type: 'CS',
+        semester: 'HK1-2020-2021',
+        department: 'K.CNTT'
+    },
+    {
+        id: 2,
+        name: 'CTDL',
+        courses_type: 'CS',
+        semester: 'HK1-2020-2021',
+        department: 'K.CNTT'
+    },
+    {
+        id: 3,
+        name: 'CNPM',
+        courses_type: 'CSN',
+        semester: 'HK1-2020-2021',
+        department: 'K.CNTT'
+    },
+    {
+        id: 4,
+        name: 'CNM',
+        courses_type: 'CN',
+        semester: 'HK1-2020-2021',
+        department: 'K.CNTT'
+    },
+    {
+        id: 5,
+        name: 'DAMH',
+        courses_type: 'CN',
+        semester: 'HK1-2020-2021',
+        department: 'K.CNTT'
+    },
+];
+
+module.exports = courses;
